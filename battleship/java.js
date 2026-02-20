1383822334fe7093d7c6691328776f2aeff7b500
Sam Myers <208719188+sam-myers444@users.noreply.github.com>
2026-02-19 19:52:57 -0500

    finished the gameover function

--- battleship/java.js
+++ battleship/java.js
@@ -1,3 +1,7 @@
+let attempts = [];
+let boardHits = [];
+let fleet = [];
+
 function ship(length) {
     return {
         length,
@@ -7,30 +11,59 @@ function ship(length) {
             this.hits++;
             if (this.hits === this.length) this.isSunk = true;
         },
+    
     };
-}
+ }
+
 
 function gameboard() {
-    let boardSize = 10;
-    let board = Array.from({ length: boardSize }, () => Array(boardSize).fill(null));
+  let boardSize = 10;
+  let board = Array.from({ length: boardSize }, () => Array(boardSize).fill(null));
 
-    function placeShip(x, y, length, isHorizontal) {
-        const coordinates = [];
-        const newShip = ship(length);
+  function isGameOver() {
+    return fleet.length > 0 && fleet.every(s => s.isSunk === true);
+  }
 
-        for (let i = 0; i < length; i++) {
-            let curRow = isHorizontal ? x : x + i;
-            let curCol = isHorizontal ? y + i : y;
+  function placeShip(x, y, length, isHorizontal) {
+    const coordinates = [];
+    const newShip = ship(length);
 
-            board[curRow][curCol] = newShip;
-            
-            coordinates.push([curRow, curCol]);
-        }
-        return coordinates;
+    for (let i = 0; i < length; i++) {
+      let curRow = isHorizontal ? x : x + i;
+      let curCol = isHorizontal ? y + i : y;
+      board[curRow][curCol] = newShip;
+      coordinates.push([curRow, curCol]);
     }
 
-    return { board, placeShip };
-}
+    fleet.push(newShip);
+    return coordinates;
+  }
 
-let submarine = new ship(5, 0, false, );
+  function receiveAttack(x, y) {
+    const attackPoint = board[x][y];
+
+    if (attackPoint !== null && attackPoint !== "hit" && attackPoint !== "miss") {
+      attackPoint.hit();           // apply hit to the ship
+      board[x][y] = "hit";
+      boardHits.push([x, y]);
+
+      if (isGameOver()) console.log("game is over");
+      return "hit";
+    }
+
+    board[x][y] = "miss";
+    attempts.push([x, y]);
+    return "miss";
+  }
+
+  return { board, placeShip, receiveAttack, isGameOver };
+};
+
+const myBoard = gameboard();
+myBoard.placeShip(0, 0, 3, true);
+console.table(myBoard.board);
+
+
+
+
 
